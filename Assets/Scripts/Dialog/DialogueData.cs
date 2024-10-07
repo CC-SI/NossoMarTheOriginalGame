123e882e20dbcb7e0af1f7ff7a699aaff8720d67
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-10-07 17:37:25 -0300

    feat : Classe que representa os dados de diálogos, contendo uma lista de grupos de diálogos.

---
+++ Assets/Scripts/Dialog/DialogueData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dialog
+{
+    /// <summary>
+    /// Classe que representa os dados de diálogos, contendo uma lista de grupos de diálogos.
+    /// </summary>
+    [Serializable]
+    public class DialogueData
+    {
+        public List<DialogueGroup> dialogueGroups;
+    }
+    
+    /// <summary>
+    /// Classe que representa um grupo de diálogos, incluindo um ID, nome do grupo e diálogos contidos.
+    /// </summary>
+    [Serializable]
+    public class DialogueGroup
+    {
+        public string id;
+        public string groupName;
+        public List<Dialogue> dialogues;
+
+        public DialogueGroup() 
+        {
+            dialogues = new List<Dialogue>();
+        }
+    }
+    
+    /// <summary>
+    /// Classe que representa um diálogo, incluindo o nome do falante e o texto do diálogo.
+    /// </summary>
+    [Serializable]
+    public class Dialogue
+    {
+        public string speaker;
+        public string text;
+
+        public Dialogue() {} 
+        
+        /// <summary>
+        /// Construtor que permite criar um diálogo com o nome do falante e o texto.
+        /// </summary>
+        /// <param name="speaker">Nome do falante.</param>
+        /// <param name="text">Texto do diálogo.</param>
+        public Dialogue(string speaker, string text)
+        {
+            this.speaker = speaker;
+            this.text = text;
+        }
+    }
+}
