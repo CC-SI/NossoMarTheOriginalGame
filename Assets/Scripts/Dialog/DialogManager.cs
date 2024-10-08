424ac82a83e1c5add524c161955d70ad9dc9ed51
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-10-07 23:20:51 -0300

    feat : Agora o player pode avança e voltar um dialogo, dialogo de decisão criado também.

--- Assets/Scripts/Dialog/DialogManager.cs
+++ Assets/Scripts/Dialog/DialogManager.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
-using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem.HID;
 using UnityEngine.UI;
 
 namespace Dialog
@@ -12,16 +14,33 @@ namespace Dialog
     /// </summary>
     public class DialogManager : MonoBehaviour
     {
+        [Header("Componentes de Interface")]
         [SerializeField] private GameObject dialogManager;
         [SerializeField] private Text speakerName;
         [SerializeField] private Text dialogueText;
         [SerializeField] private GameObject joystick;
-        [SerializeField] private Button advanceButton;
         [SerializeField] private GameObject helpDuck;
+
+        [Header("Botões de Navegação")]
+        [SerializeField] private Button advanceButton;
+        [SerializeField] private Button backButton;
+        [SerializeField] private GameObject navNextGameObject;
+        [SerializeField] private GameObject navBackGameObject;
+        [SerializeField] private Button closedDialog;
+
+        [Header("Decisões de Diálogo")]
+        [SerializeField] private GameObject dialogOptions;
+        [SerializeField] private Button btnYes;
+        [SerializeField] private Button btnNo;
+
+        [Header("Mensagens Especiais")]
+        [SerializeField] private GameObject messageFindFri;
         
         private List<DialogueGroup> dialogueGroups = new List<DialogueGroup>(); // Lista de grupos de diálogos.
         private int currentDialogueIndex = 0; // Índice do diálogo atual dentro do grupo atual.
         private int currentGroupIndex = 0; // Índice do grupo de diálogos atual.
+
+        private bool isFriCollect = false; // Verifica se o jogador coletou a pá.
         
         /// <summary>
         /// Inicializa o gerenciador de diálogos ao iniciar o jogo.
@@ -29,8 +48,13 @@ namespace Dialog
         private void Start()
         {
             HideDialog();
+            messageFindFri.SetActive(false);
             LoadDialogues();
             advanceButton.onClick.AddListener(OnAdvanceButtonClicked);
+            btnYes.onClick.AddListener(OnAdvanceButtonClicked);
+            btnNo.onClick.AddListener(HideDialog);
+            backButton.onClick.AddListener(ShowPreviousDialog);
+            closedDialog.onClick.AddListener(HideDialog);
         }
         
         /// <summary>
@@ -56,6 +80,22 @@ namespace Dialog
                 dialogueGroups = dialogueData.dialogueGroups;
             }
         }
+
+        /// <summary>
+        /// Volta para o diálogo anterior da lista.
+        /// </summary>
+        private void ShowPreviousDialog()
+        {
+            if (currentDialogueIndex > 0)
+            {
+                currentDialogueIndex -= 2;
+                ShowNextDialog();
+            }
+            else
+            {
+                HideDialog();
+            }
+        }
         
         /// <summary>
         /// Mostra o próximo diálogo da lista.
@@ -72,9 +112,10 @@ namespace Dialog
                 {
                     dialogManager.SetActive(true); // Ativa o painel de diálogo.
                     joystick.SetActive(false); // Oculta o joystick durante o diálogo.
+                    messageFindFri.SetActive(false);
                     
                     var currentDialogue = currentGroup.dialogues[currentDialogueIndex]; // Obtém o diálogo atual.
-
+                    
                     // Atualiza o texto do falante.
                     if (speakerName != null)
                     {
@@ -88,16 +129,49 @@ namespace Dialog
                     }
 
                     // Verifica se o diálogo atual é sobre o pato enterrado e ativa o objeto de ajuda, se necessário.
-                    if (dialogueGroups[currentGroupIndex].id == "patoenterrado" && currentDialogue.speaker == "Pato")
+                    if (currentDialogue.id == "pato_enterrado_pedindo_ajuda")
                     {
                         helpDuck.SetActive(true);
+                        dialogOptions.SetActive(true);
+                        navNextGameObject.SetActive(false);
                     }
                     else
                     {
                         helpDuck.SetActive(false);
+                        dialogOptions.SetActive(false);
+                        navNextGameObject.SetActive(true);
                     }
 
-                    currentDialogueIndex++; // Avança para o próximo diálogo.
+                    // Pula em 6 segundos caso o player não pulou pelo botão de avançar
+                    if (currentDialogue.id == "pato_enterrado_pedindo_ajuda")
+                    {
+                        StartCoroutine(CountAndAdvanceDialog());
+                    }
+
+                    // Se o diálogo atual for "encontrar_pa" e isFriCollect for false, não avança.
+                    if (currentDialogue.id == "encontrar_pa")
+                    {
+                        if (isFriCollect)
+                        {
+                            navBackGameObject.SetActive(true); // Permite voltar.
+                            helpDuck.SetActive(false); // Oculta o objeto de ajuda.
+                            dialogOptions.SetActive(false); // Oculta as opções de diálogo.
+                            messageFindFri.SetActive(false); // Oculta a mensagem que o player precisa encontrar a pá.
+                            currentDialogueIndex++; // Avança para o próximo diálogo se isFriCollect for true.
+                        }
+                        else
+                        {
+                            navNextGameObject.SetActive(false); // Desativa o botão de avanço.
+                            navBackGameObject.SetActive(true); // Permite voltar.
+                            helpDuck.SetActive(false); // Oculta o objeto de ajuda.
+                            dialogOptions.SetActive(false); // Oculta as opções de diálogo.
+                            messageFindFri.SetActive(true); // Exibe a mensagem que o player precisa encontrar a pá.
+                        }
+                    }
+                    else
+                    {
+                        currentDialogueIndex++; // Avança para o próximo diálogo, exceto para "encontrar_pa".
+                    }
                 }
                 else
                 {
@@ -117,6 +191,19 @@ namespace Dialog
                 HideDialog(); // Oculta o diálogo se o gerenciador não estiver ativo.
             }
         }
+
+        /// <summary>
+        /// Conta 6 segundos e avança o diálogo.
+        /// </summary>
+        private IEnumerator CountAndAdvanceDialog()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                yield return new WaitForSeconds(1f); // Aguarda 1 segundo.
+            }
+            
+            ShowNextDialog();
+        }
         
         /// <summary>
         /// Oculta o painel de diálogo e redefine os índices dos diálogos.
@@ -125,13 +212,22 @@ namespace Dialog
         {
             if (dialogManager != null)
             {
-                dialogManager.SetActive(false);
-                currentDialogueIndex = 0;
-                currentGroupIndex = 0;
+                dialogManager.SetActive(false); // Oculta o painel de diálogo.
+                currentDialogueIndex = 0; // Reseta o índice do diálogo atual.
+                currentGroupIndex = 0; // Reseta o índice do grupo de diálogos.
                 
-                joystick.SetActive(true);
-                
-                helpDuck.SetActive(false);
+                joystick.SetActive(true); // Exibe o joystick.
+                helpDuck.SetActive(false); // Oculta o objeto de ajuda.
+                dialogOptions.SetActive(false); // Oculta as opções de diálogo.
+
+                if (isFriCollect == false && messageFindFri.activeSelf) // Se a pá não foi coletada e a mensagem está ativa.
+                {
+                    messageFindFri.SetActive(true); // Exibe a mensagem que o player precisa encontrar a pá.
+                }
+                else
+                {
+                    messageFindFri.SetActive(false); // Oculta a mensagem que o player precisa encontrar a pá.
+                }
             }
         }
         
