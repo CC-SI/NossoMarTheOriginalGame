4671f53ff62870d716d315c80d308673d62befa0
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-10-10 20:26:44 -0300

    refactor: tirando linhas desnecessárias do script Duck

--- Assets/Scripts/Duck/Duck.cs
+++ Assets/Scripts/Duck/Duck.cs
@@ -1,98 +1,47 @@
-using System;
-using Actors;
 using Interaction;
 using Player;
 using TMPro;
 using UnityEngine;
-using UnityEngine.AI;
-using UnityEngine.Events;
 
-public class Duck : InteractableObject, IInteraction, IMovement
+public class Duck : InteractableObject, IInteraction
 {
-    [field: Header("Componentes Externos")]
-    [SerializeField] private Transform alvo;
     [SerializeField] private TMP_Text countDucks;
     [SerializeField] private AudioClip clip;
+    
     private Movement movement;
-    
-    [field: Header("Componentes Internos")]
-    private Rigidbody2D rb;
     private Collider2D colisor;
-    private Animator animator; 
-    private NavMeshAgent agent;
     
-    [field: Header("Eventos")]
-    [field: SerializeField] public UnityEvent<Vector2> OnMoved {get; private set;}
-    
-    [field: Header("Lógicos")]
-    private static int currentDuck = 0;
-    public bool isFollowing; 
+    private static int currentDuck = 0; 
     private AudioSource audioSource;
     
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         colisor = GetComponent<Collider2D>();
-        animator = GetComponent<Animator>();
-        agent = GetComponent<NavMeshAgent>();
-
         movement = GetComponent<Movement>();
-        
-        agent.updateRotation = false;
-        agent.updateUpAxis = false;
     
-        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
         audioSource.clip = clip;
     
         AddObject(colisor, this);
-        
-        if (movement != null)
-        {
-            movement.OnMoved.AddListener(OnMovedHandler);
-        }   
     }
     
-    /// <summary>
-    /// Função para o pato começar a seguir o jogador.
-    /// </summary>
-    public void StartFollowing() 
+    private void StartFollowing()
     {
-		if (PlayerBehaviour.Instance)
-			alvo = PlayerBehaviour.Instance.GetFollowTarget(this);
-        
-		isFollowing = true;
-        
+        if (PlayerBehaviour.Instance)
+        {
+            Transform alvo = PlayerBehaviour.Instance.GetFollowTarget(this);
+            movement.SetFollowTarget(alvo);
+        }
+
         currentDuck++;
         countDucks.text = currentDuck.ToString();
         
         audioSource.Play(); 
-        
-        movement.SetFollowTarget(alvo);
-    }
-    
-    public void OnPlayerInteraction()
-    {
-        if (!isFollowing)
-        {
-            StartFollowing();
-            RemoveObject(colisor);
-        }
-    }
-    
-    public bool CanInteract()
-    {
-        // Retorna se o pato pode ser interagido (se não estiver seguindo)
-        return !isFollowing;
     }
 
-    private void OnMovedHandler(Vector2 direction)
+    void IInteraction.OnPlayerInteraction()
     {
-        // Gira o pato para a esquerda ou direita com base na direção do movimento
-        if (direction.x != 0)
-        {
-            Vector3 scale = transform.localScale;
-            scale.x = Mathf.Sign(direction.x) * Mathf.Abs(scale.x);
-            transform.localScale = scale;
-        }
+        StartFollowing();
+        RemoveObject(colisor);
     }
 }
