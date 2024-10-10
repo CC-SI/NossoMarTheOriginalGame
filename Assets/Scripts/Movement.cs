da9fdbb1b95be156f92499e14274ebca32368d31
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-10-10 20:28:14 -0300

    refactor: removendo linhas desnecessárias no script Movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,16 +5,17 @@ using UnityEngine.Events;
 
 public class Movement : MonoBehaviour, IMovement
 {
-    [SerializeField] private FixedJoystick joystick;
-    [SerializeField] private float velocidadeMovimento = 30f;
+    
     [field: Header("Eventos")]
     [field: SerializeField] public UnityEvent<Vector2> OnMoved { get; private set; }
+    
+    [SerializeField] private FixedJoystick joystick;
 
     private NavMeshAgent navMeshAgent;
     private Transform followTarget;
     private Animator animator;
 
-    void Start()
+    private void Start()
     {
         // Inicializa o NavMeshAgent e o Animator
         navMeshAgent = GetComponent<NavMeshAgent>();
@@ -23,37 +24,36 @@ public class Movement : MonoBehaviour, IMovement
         animator = GetComponent<Animator>();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         // Se há um alvo a seguir, chama FollowTarget
-        if (followTarget != null)
+        if (followTarget)
         {
             FollowTarget();
+            return;
         }
         // Caso contrário, usa o joystick para movimentação
-        else if (joystick != null)
+        if (!joystick) return;
+        float horizontal = joystick.Horizontal;
+        float vertical = joystick.Vertical;
+
+        var direcao = new Vector3(horizontal, vertical, 0).normalized;
+
+        if (direcao.magnitude >= 0.1f)
         {
-            float horizontal = joystick.Horizontal;
-            float vertical = joystick.Vertical;
-
-            Vector3 direcao = new Vector3(horizontal, vertical, 0).normalized;
-
-            if (direcao.magnitude >= 0.1f)
-            {
-                Vector3 targetPosition = transform.position + (direcao * velocidadeMovimento * Time.deltaTime);
-                navMeshAgent.SetDestination(targetPosition);
-                OnMoved.Invoke(new Vector2(horizontal, vertical) * velocidadeMovimento);
-            }
-            else
-            {
-                navMeshAgent.velocity = Vector3.zero;
-                navMeshAgent.ResetPath();
-                OnMoved.Invoke(Vector2.zero);
-            }
-
-            // Atualiza a animação de movimento
-            animator.SetBool("isWalking", navMeshAgent.velocity.magnitude > 0);
+            Vector3 targetPosition = transform.position + (direcao * (navMeshAgent.speed * Time.deltaTime));
+            navMeshAgent.SetDestination(targetPosition);
+            OnMoved.Invoke(new Vector2(horizontal, vertical) * navMeshAgent.speed);
         }
+        else
+        {
+            navMeshAgent.velocity = Vector3.zero;
+            navMeshAgent.ResetPath();
+            OnMoved.Invoke(Vector2.zero);
+        }
+
+        // Atualiza a animação de movimento
+        animator.SetBool("isWalking", navMeshAgent.velocity.magnitude > 0);
     }
 
 
@@ -65,19 +65,13 @@ public class Movement : MonoBehaviour, IMovement
 
     public void FollowTarget()
     {
-        if (followTarget != null)
-        {
-            Vector2 posicaoAlvo = followTarget.position;
-            Vector2 posicaoPato = transform.position;
+        Vector2 posicaoAlvo = followTarget.position;
+        Vector2 posicaoPato = transform.position;
 
-            var direcao = ((posicaoAlvo - posicaoPato).normalized) * navMeshAgent.velocity.magnitude;
+        var direcao = ((posicaoAlvo - posicaoPato).normalized) * navMeshAgent.velocity.magnitude;
 
-            navMeshAgent.SetDestination(posicaoAlvo);
+        navMeshAgent.SetDestination(posicaoAlvo);
             
-            // Atualiza a animação de movimento
-            animator.SetBool("isWalking", navMeshAgent.velocity.magnitude > 0);
-            
-            OnMoved.Invoke(direcao);
-        }
+        OnMoved.Invoke(direcao);
     }
 }
