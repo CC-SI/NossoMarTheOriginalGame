35c3668871e1645682a6e131c09e516117ec735a
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-10-20 12:39:35 -0300

    feat : script para patos com narrativas

---
+++ Assets/Scripts/Duck/DuckDialog.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Dialog;
+using Interaction;
+
+namespace Duck
+{
+    public class DuckDialog : DuckBehavior
+    {
+        public static DuckDialog Instance { get; private set; }
+
+
+        private Dictionary<string, string> mapDialogues = new Dictionary<string, string>()
+        {
+            { "DuckBuriedTag", "patoenterrado" }
+        };
+        
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+        
+        public override void OnPlayerInteraction()
+        {
+            if (!isFollowing)
+            {
+                if (mapDialogues.TryGetValue(tag, out string dialogueId))
+                {
+                    DialogManager.Instance.StartDialogue(dialogueId);
+                    
+                    if (DialogManager.Instance.IsDialogFinshed)
+                    {
+                        StartFollowing();
+                    }
+                }
+            }
+        }
+
+        public void StartFollowing()
+        {
+            base.OnPlayerInteraction();
+            HideCaptureButton();
+        }
+    
+        private void HideCaptureButton()
+        {
+            InteractionUIButton interactionUIButton = FindObjectOfType<InteractionUIButton>();
+            if (interactionUIButton != null)
+            {
+                interactionUIButton.HideButton();
+            }
+        }
+    }
+}
