8b04e51480f9f6deb093fe034449beb94ae29cd2
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-10-25 17:17:32 -0300

    feat : class do dialogo

---
+++ Assets/Scripts/Dialog/Dialogo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dialog
+{
+    [Serializable]
+    public class Dialogo
+    {
+        public string id;
+        public string speaker;
+        public string texto;
+        public TypeDialogEnum typeDialog;
+    }
+}
