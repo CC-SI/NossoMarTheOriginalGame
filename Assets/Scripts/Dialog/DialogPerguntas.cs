48af95a8b4d6f48759e1f8726566559fb7f92139
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-10-25 17:20:06 -0300

    delete : removendo scrpts

--- Assets/Scripts/Dialog/DialogPerguntas.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-[CreateAssetMenu] public class DialogPerguntas : DialogObject
-{
-    public List<string> perguntas = new();
-    
-    
-    
-}
