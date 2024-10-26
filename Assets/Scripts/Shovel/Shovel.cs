9f035bafd736591731609a68d265304cf44891f0
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-10-26 00:05:45 -0300

    feat : tirando logica de movimentação da pa e verificando se pa foi coletada para avançar o dialogo

--- Assets/Scripts/Shovel/Shovel.cs
+++ Assets/Scripts/Shovel/Shovel.cs
@@ -1,77 +1,35 @@
-using Actors;
 using Dialog;
 using Interaction;
-using Player;
 using UnityEngine;
-using UnityEngine.AI;
-using UnityEngine.Events;
 
-namespace Shovel
-{
-    public class Shovel : InteractableObject, IInteraction, IMovement
+
+    public class Shovel : InteractableObject, IInteraction
     {
-        [field: Header("Componentes Externos")]
-        [SerializeField] private Transform alvo;
-        private Movement movement;
-
         [field: Header("Componentes Internos")]
         private Rigidbody2D rb;
         private Collider2D colisor;
-        private NavMeshAgent agent;
-
-        [field: Header("Eventos")]
-        [field: SerializeField] public UnityEvent<Vector2, bool> OnMoved { get; private set; }
 
         [field: Header("Lógicos")]
-        public bool isFollowing;
+        public bool isCollected = false;
         
-        private bool isCollected = false;
         private void Start()
         {
             rb = GetComponent<Rigidbody2D>();
             colisor = GetComponent<Collider2D>();
-            agent = GetComponent<NavMeshAgent>();
-            movement = GetComponent<Movement>();
-
-            agent.updateRotation = false;
-            agent.updateUpAxis = false;
-
             AddObject(colisor, this);
-        }
-    
-        /// <summary>
-        /// Função para a pá começar a seguir o jogador.
-        /// </summary>
-        public void StartFollowing()
-        {
-            if (PlayerBehaviour.Instance)
-            {
-                alvo = PlayerBehaviour.Instance.transform; 
-                isFollowing = true;
-                movement.SetFollowTarget(alvo); 
-            }
-        }
-
-        private void FixedUpdate()
-        {
-            if (isFollowing)
-            {
-                movement.FollowTarget();
-            }
         }
 
         public void OnPlayerInteraction()
         {
-            if (!isFollowing && !isCollected)
+            if (!isCollected)
             {
-                StartFollowing(); 
-                RemoveObject(colisor);
-            
-                DialogManager.Instance.CollectPa();
+                Debug.Log("Capturou a pá");
+                
                 isCollected = true;
                 
                 gameObject.SetActive(false);
+                
+                DialogManager.Instance.AdvanceDialogWithoutUI();
             }
         }
     }
-}
