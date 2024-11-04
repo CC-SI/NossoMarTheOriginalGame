2286c213df557ff79327666df2ed15a646645311
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-11-04 13:31:28 -0300

    feat : avançando dialogo silenciosamente

--- Assets/Scripts/Duck/DuckDialog.cs
+++ Assets/Scripts/Duck/DuckDialog.cs
@@ -7,18 +7,30 @@ namespace Duck
     public class DuckDialog : DuckBehavior
     {
         [SerializeField] private DialogManager dialogManager;
+        [SerializeField] private ObjectToBeCaptured objectToBeCaptured;
+        
+        public bool isDuckAguaCoco;
         
         public override void OnPlayerInteraction()
         {
             if (!isFollowing)
             {
+                if (objectToBeCaptured != null && isDuckAguaCoco && objectToBeCaptured.IsAllCapturedCocos)
+                {
+                    dialogManager.AvancarDialogoSilenciosamente();
+                }
                 dialogManager.StartDialog();
             }
         }
-
+        
         public override void StartFollowing()
         {
-            Debug.Log("DuckDialog: StartFollowing");
+            if (isDuckAguaCoco)
+            {
+                Debug.Log("Pato agua de coco capturado ");
+            }
+            
+            // Debug.Log("DuckDialog: StartFollowing");
             base.StartFollowing();
             HideCaptureButton();
         }
