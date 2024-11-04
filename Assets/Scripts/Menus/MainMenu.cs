8a493fe8712d8497d851015435153cb39fd843f3
Ryushuke <ryushuke@hotmail.com>
2024-11-04 16:41:41 -0300

    feature: Menu Principal funcional.
    - Exceto pela serialização de dados.

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menus
+{
+	public class MainMenu : MenuBehaviour
+	{
+		[Header("Componentes")]
+		[SerializeField]
+		Button continueButton;
+		[SerializeField]
+		Button newGameButton;
+		[SerializeField]
+		Button optionsButton;
+		[SerializeField]
+		Button exitButton;
+
+		void Continue()
+		{
+			// Carregar os dados serializados.
+		}
+		
+		void NewGame()
+		{
+			// Carregar a cena de gameplay.
+		}
+		
+		void Options()
+		{
+			Invoke(nameof(ShowSettings), Delay);
+		}
+		
+		void Exit()
+		{
+			GameManager.Exit();
+		}
+		
+		void Awake()
+		{
+			MainMenu = this;
+			continueButton.onClick.AddListener(Continue);
+			newGameButton.onClick.AddListener(NewGame);
+			optionsButton.onClick.AddListener(Options);
+			exitButton.onClick.AddListener(Exit);
+		}
+
+		void Start()
+		{
+			optionsButton.interactable = SettingsMenu;
+			continueButton.gameObject.SetActive(GameManager.HasGameData);
+		}
+
+#if UNITY_EDITOR
+		void Reset()
+		{
+			var buttons = GetComponentsInChildren<Button>(true);
+			
+			continueButton = buttons[0];
+			newGameButton = buttons[1];
+			optionsButton = buttons[2];
+			exitButton = buttons[3];
+		}
+#endif
+	}
+}
---
+++ Assets/Scripts/Menus/MenuBehaviour.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+namespace Menus
+{
+	public abstract class MenuBehaviour : MonoBehaviour
+	{
+		protected const float Delay = .25f;
+
+		protected static SettingsMenu SettingsMenu;
+		protected static MainMenu MainMenu;
+		protected static PauseMenu PauseMenu;
+		
+		bool IsOpen
+		{
+			get => gameObject.activeInHierarchy;
+			set => gameObject.SetActive(value);
+		}
+		
+		public void ShowSettings()
+		{
+			if (SettingsMenu)
+				SettingsMenu.Open();
+			
+			Close();
+		}
+		
+		public void ShowMainMenu()
+		{
+			if(MainMenu)
+				MainMenu.Open();
+			else
+				GameManager.LoadMainMenu();
+			
+			Close();
+		}
+		
+		public void ShowPauseMenu()
+		{
+			if(PauseMenu)
+				PauseMenu.Open();
+			
+			Close();
+		}
+
+		protected void Close()
+		{
+			IsOpen = false;
+		}
+
+		void Open()
+		{
+			IsOpen = true;
+		}
+	}
+}
