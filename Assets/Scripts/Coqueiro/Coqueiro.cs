106e09a681dfdf72de91d06ac590bc1db5e3fdc5
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-11-04 13:26:12 -0300

    feat : script do coqueiro

---
+++ Assets/Scripts/Coqueiro/Coqueiro.cs
@@ -0,0 +1,19 @@
+﻿using Dialog.Manager;
+using UnityEngine;
+
+namespace Coqueiro
+{
+    public class Coqueiro : DuckBehavior
+    {
+        [SerializeField] private DialogManager dialogManager;
+        
+        public override void OnPlayerInteraction()
+        {
+            if (!isFollowing)
+            {
+                Debug.Log("Player interagiu com o coqueiro");
+                dialogManager.StartDialog();
+            }
+        }
+    }
+}
