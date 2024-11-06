703bd3fde8ca47697c25d0b08427c07647ffba76
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-11-06 16:48:01 -0300

    fix: armazena cocos capturados em uma lista para permitir novos diálogos sem finalizar o diálogo do Pato Água de Coco

--- Assets/Scripts/Shovel/ObjectToBeCaptured.cs
+++ Assets/Scripts/Shovel/ObjectToBeCaptured.cs
@@ -1,51 +1,54 @@
-using System.Collections.Generic;
 using Dialog.Manager;
 using Interaction;
 using UnityEngine;
+
+using System.Collections.Generic;
 
 public class ObjectToBeCaptured : InteractableObject, IInteraction
 {
     [field: Header("Componentes Internos")]
     private Rigidbody2D rb;
     private Collider2D colisor;
-    
+
     [SerializeField] private DialogManager dialogManager;
     [SerializeField] private DialogUIManager dialogUIManager;
 
     [SerializeField] private bool IsCocoCaptured;
-    
+
     private static int currentCoco = 0;
+    private List<ObjectToBeCaptured> capturedCocos = new List<ObjectToBeCaptured>();
 
     public bool IsAllCapturedCocos;
-    
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         colisor = GetComponent<Collider2D>();
         AddObject(colisor, this);
     }
-    
+
     public void OnPlayerInteraction()
     {
-        Debug.Log(gameObject.name + " foi capturado");
-        
         if (IsCocoCaptured)
         {
-            Debug.Log("Coco capturado");
             gameObject.SetActive(false);
             CocosCapturados();
 
             if (currentCoco >= 2)
             {
                 IsAllCapturedCocos = true;
+                if (dialogManager != null)
+                {
+                    dialogManager.AvancarDialogoSilenciosamente();
+                }
             }
         }
-        
+
         if (dialogManager != null)
         {
             dialogManager.AvancarDialogoSilenciosamente();
         }
-        
+
         if (dialogUIManager != null)
         {
             dialogUIManager.ShowMensagemObjetoPego(true);
@@ -55,6 +58,8 @@ public class ObjectToBeCaptured : InteractableObject, IInteraction
     private void CocosCapturados()
     {
         currentCoco++;
-        Debug.Log(currentCoco);
+        capturedCocos.Add(this); 
     }
+    
+    
 }
