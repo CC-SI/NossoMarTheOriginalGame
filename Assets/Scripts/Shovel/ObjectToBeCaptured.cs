6de5cf8472cb8dbd6b5363fd3a5c98e27fb92f02
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-11-08 17:42:41 -0300

    feat : guardando cocos capturados em uma lista

--- Assets/Scripts/Shovel/ObjectToBeCaptured.cs
+++ Assets/Scripts/Shovel/ObjectToBeCaptured.cs
@@ -1,24 +1,22 @@
+using System.Collections.Generic;
 using Dialog.Manager;
 using Interaction;
 using UnityEngine;
-
-using System.Collections.Generic;
 
 public class ObjectToBeCaptured : InteractableObject, IInteraction
 {
     [field: Header("Componentes Internos")]
     private Rigidbody2D rb;
     private Collider2D colisor;
-
+    
     [SerializeField] private DialogManager dialogManager;
     [SerializeField] private DialogUIManager dialogUIManager;
 
     [SerializeField] private bool IsCocoCaptured;
+    
+    private static List<ObjectToBeCaptured> cocosCapturadosList = new();
 
-    private static int currentCoco = 0;
-    private List<ObjectToBeCaptured> capturedCocos = new List<ObjectToBeCaptured>();
-
-    public bool IsAllCapturedCocos;
+    public bool IsAllCapturedCocos => cocosCapturadosList.Count >= 2;  
 
     private void Start()
     {
@@ -26,29 +24,23 @@ public class ObjectToBeCaptured : InteractableObject, IInteraction
         colisor = GetComponent<Collider2D>();
         AddObject(colisor, this);
     }
-
+    
     public void OnPlayerInteraction()
     {
+        Debug.Log(gameObject.name + " foi capturado");
+        
         if (IsCocoCaptured)
         {
+            Debug.Log("Coco capturado");
             gameObject.SetActive(false);
             CocosCapturados();
-
-            if (currentCoco >= 2)
-            {
-                IsAllCapturedCocos = true;
-                if (dialogManager != null)
-                {
-                    dialogManager.AvancarDialogoSilenciosamente();
-                }
-            }
         }
-
+        
         if (dialogManager != null)
         {
             dialogManager.AvancarDialogoSilenciosamente();
         }
-
+        
         if (dialogUIManager != null)
         {
             dialogUIManager.ShowMensagemObjetoPego(true);
@@ -57,9 +49,12 @@ public class ObjectToBeCaptured : InteractableObject, IInteraction
 
     private void CocosCapturados()
     {
-        currentCoco++;
-        capturedCocos.Add(this); 
+        cocosCapturadosList.Add(this); 
+        Debug.Log("Cocos capturados: " + cocosCapturadosList.Count);
     }
     
-    
+    public static List<ObjectToBeCaptured> GetCocosCapturadosList()
+    {
+        return cocosCapturadosList;
+    }
 }
