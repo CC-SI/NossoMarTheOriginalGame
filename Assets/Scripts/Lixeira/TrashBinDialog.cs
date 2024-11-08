9f97e4d013e2c3aa93f801dcd6a4e97208760e29
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-11-08 14:38:43 -0300

    raw: adicionando os audios recortados para se encaixarem no loop
    Co-Authored-By: GuilhermeFortunato3466 <131789690+GuilhermeFortunato@users.noreply.github.com>

---
+++ Assets/Scripts/Lixeira/TrashBinDialog.cs
@@ -0,0 +1,16 @@
+﻿using Dialog.Manager;
+using Interaction;
+using UnityEngine;
+
+namespace Lixeira
+{
+    public class TrashBinDialog : DuckBehavior
+    {
+        [SerializeField] private DialogManager dialogManager;
+
+        public override void OnPlayerInteraction()
+        {
+            dialogManager.StartDialog();
+        }
+    }
+}
