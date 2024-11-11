5edbe6c0701bf99db311227a5b6672f82a071b8d
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-11-11 14:34:38 -0300

    feat : verificando se o usuario esta no pc ou mobile com base com valores passados na tela

---
+++ Assets/Scripts/DefinitionSoftware/WhatAreYouSoftware.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Dialog.DefinitionSoftware
+{
+    public class WhatAreYouSoftware : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.touchCount > 0)
+            {
+                PlayerPrefs.SetString("DeviceType", "Mobile");
+            } 
+            else if (Input.anyKey)
+            {
+                PlayerPrefs.SetString("DeviceType", "Pc");
+            }
+            
+            PlayerPrefs.Save();
+        }
+    }
+}
