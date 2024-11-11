ac92303a98be7252a7c2fab85d5cb8c369cb6b3e
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-11-11 14:43:36 -0300

    feat : adicionando cena do tutorial

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -19,7 +19,7 @@ namespace Menus
 			=> ButtonInvoke(() => GameManager.LoadGame(true));
 
 		void NewGame()
-			=> ButtonInvoke(() => GameManager.LoadGame());
+			=> ButtonInvoke(() => GameManager.LoadNewGame());
 
 		void Options()
 		{
