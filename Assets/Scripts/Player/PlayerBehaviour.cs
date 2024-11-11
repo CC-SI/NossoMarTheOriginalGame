72ccc8219a7dd79e71a5e81193217e0fab2bae01
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-11-11 14:47:04 -0300

    feat : Player nn pode se mover enquanto acontece o dialogo

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Dialog.Manager;
 using Duck;
 using Interaction;
 using Serialization;
@@ -6,68 +7,91 @@ using UnityEngine;
 
 namespace Player
 {
-	public class PlayerBehaviour : MonoBehaviour, ISerializable
-	{
-		readonly List<DuckBehavior> ducks = new();
+    public class PlayerBehaviour : MonoBehaviour, ISerializable
+    {
+        readonly List<DuckBehavior> ducks = new();
 
-		[field: Header("Componentes")]
-		[field: SerializeField]
-		public Movement Movement { get; private set; }
-		[field: SerializeField]
-		public InteractableZone InteractableZone { get; private set; }
-		
-		public static PlayerBehaviour Instance { get; private set; }
+        [field: Header("Componentes")]
+        [field: SerializeField]
+        public Movement Movement { get; private set; }
+        [field: SerializeField]
+        public InteractableZone InteractableZone { get; private set; }
 
-		public Transform GetFollowTarget(DuckBehavior duckBehavior)
-		{
-			Transform target = GetFollowTarget();
+        [field: SerializeField] private List<DialogManager> dialogManagers;
 
-			if (!ducks.Contains(duckBehavior))
-				ducks.Add(duckBehavior);
+        public static PlayerBehaviour Instance { get; private set; }
 
-			return target;
-		}
-
-		public Transform GetFollowTarget()
-		{
-			if (ducks.Count < 1
-			    || ducks.Count % 6 == 0)
-				return transform;
-
-			return ducks[^1].transform; 
-		}
+        private void Update()
+        {
+            Movement.enabled = !IsAnyDialogActive();
+        }
         
-		public void Save(SaveData data){
-			data.playerPosition = transform.position;
-		}
-        
-		public void Load(SaveData data){
-			transform.position = data.playerPosition;
-		}
+        private bool IsAnyDialogActive()
+        {
+            if (dialogManagers == null) return false;
+            
+            foreach (var dialogManager in dialogManagers)
+            {
+                if (dialogManager != null && dialogManager.IsDialogActive)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
-		void Awake()
-		{
-			if (!Instance)
-			{
-				Instance = this;
-				GameManager.Subscribe(this);
-				return;
-			}
-			
-			Destroy(gameObject);
-		}
+        public Transform GetFollowTarget(DuckBehavior duckBehavior)
+        {
+            Transform target = GetFollowTarget();
 
-		private void OnDestroy()
-		{
-			GameManager.Unsubscribe(this);
-		}
+            if (!ducks.Contains(duckBehavior))
+                ducks.Add(duckBehavior);
+
+            return target;
+        }
+
+        public Transform GetFollowTarget()
+        {
+            if (ducks.Count < 1 || ducks.Count % 6 == 0)
+                return transform;
+
+            return ducks[^1].transform; 
+        }
+
+        public void Save(SaveData data)
+        {
+            if (data == null) return;
+            data.playerPosition = transform.position;
+        }
+
+        public void Load(SaveData data)
+        {
+            transform.position = data.playerPosition;
+        }
+
+        void Awake()
+        {
+            if (!Instance)
+            {
+                Instance = this;
+                GameManager.Subscribe(this);
+                return;
+            }
+
+            Destroy(gameObject);
+        }
+
+        private void OnDestroy()
+        {
+            GameManager.Unsubscribe(this);
+        }
 
 #if UNITY_EDITOR
-		void Reset()
-		{
-			Movement = GetComponent<Movement>();
-			InteractableZone = GetComponentInChildren<InteractableZone>(true);
-		}
+        void Reset()
+        {
+            Movement = GetComponent<Movement>();
+            InteractableZone = GetComponentInChildren<InteractableZone>(true);
+        }
 #endif
-	}
+    }
 }
