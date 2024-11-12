15ea928968c3a4cf0bc65f3e88b1273602da4399
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-11-12 03:21:22 -0300

    feat: criando a classe VideoController para mexer no video da cena

---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    public string videoFileName = "meu_video.mp4";
+    private VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        if (!File.Exists(GetVideoPath())) return;
+        
+        videoPlayer.url = GetVideoPath();
+        videoPlayer.Play();
+    }
+
+    string GetVideoPath()
+    {
+        var videoPath = Path.Combine(Application.streamingAssetsPath, videoFileName);
+        return videoPath;
+    }
+}
