45e4c40fbe32af1f8018d410e036f37c9221dbd2
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-11-12 12:57:16 -0300

    refactor: ao clicar em novo jogo será chamado a cena da introdução antes do tutorial

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -19,7 +19,7 @@ namespace Menus
 			=> ButtonInvoke(() => GameManager.LoadGame(true));
 
 		void NewGame()
-			=> ButtonInvoke(() => GameManager.LoadTutorial());
+			=> ButtonInvoke(() => GameManager.LoadIntro());
 
 		void Options()
 		{
