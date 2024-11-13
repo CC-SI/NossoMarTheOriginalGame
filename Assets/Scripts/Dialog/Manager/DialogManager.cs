0f48f4ad2df497c89ca3361733c6e49d1d06642e
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-11-13 15:13:11 -0300

    feat : função que fecha e finaliza o dialogo

--- Assets/Scripts/Dialog/Manager/DialogManager.cs
+++ Assets/Scripts/Dialog/Manager/DialogManager.cs
@@ -143,11 +143,38 @@ namespace Dialog.Manager
                 PlayerBehaviour.Instance.Movement.enabled = true;
             }
         }
+        
+        public void FecharDialogoEFinalizar()
+        {
+            IsDialogActive = false;
+            dialogUIManager.ShowDialog(false);
 
+            if (!isTutorial)
+            {
+                PlayerBehaviour.Instance.Movement.enabled = true;
+            }
+            
+            AvancarDialogo();
+        }
     
         private void ResetDialog()
         {
             dialogObject.ResetDialog();
+        }
+        
+        public void FinalizarTutorial()
+        {
+            IsDialogActive = false;
+            dialogUIManager.ShowDialog(false);
+
+            if (!isTutorial)
+            {
+                PlayerBehaviour.Instance.Movement.enabled = true;
+            }
+            
+            ResetDialog();
+            
+            Debug.Log("Dialogo finalizado");
         }
     }
 }
