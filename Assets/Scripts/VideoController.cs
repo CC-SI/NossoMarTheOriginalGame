6bca24d73fddb1f5a8269f33454d364f9fc8f655
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-11-13 15:24:55 -0300

    refactor: usando corrotina para verificar se o VideoPlayer está preparado
    - Antes, para quem estava no editor, era verificada a existência do caminho do vídeo no diretório do jogo e, caso não existisse, seria carregada a próxima cena do jogo (tutorial).
    - Agora essa verificação foi removida e substituída por uma corrotina. Seguindo a lógica escrita nela, se em até 5 segundos após o inicio da preparação do vídeo o mesmo não estiver preparado, será carregada a próxima cena.
    - Benefícios: Essa verificação acontecerá em qualquer plataforma, diferente da utilização do File.Exists(), que só funciona como esperado em algumas plataformas (Linux e Windows).
    - A lógica da verificação não é a mesma, pois antes era verificado o caminho do vídeo no diretório do jogo, e agora está sendo verificado se o VideoPlayer conseguiu preparar o vídeo. Entretanto, o objetivo da verificação continua sendo a mesma, trocar de cena se não for possível iniciar o vídeo.

--- Assets/Scripts/VideoController.cs
+++ Assets/Scripts/VideoController.cs
@@ -1,43 +1,58 @@
-﻿using System;
+﻿using System.Collections;
 using System.IO;
-using TMPro;
 using UnityEngine;
 using UnityEngine.Video;
 
 public class VideoController : MonoBehaviour
 {
     [SerializeField] private string videoFileName;
-    [SerializeField] private VideoPlayer videoPlayer;
     [SerializeField] private string nextScene;
+    [SerializeField] private VideoPlayer videoPlayer;
+
+    void Awake()
+    {
+        videoPlayer.loopPointReached += EndReached;
+        videoPlayer.prepareCompleted += PlayVideo;
+    }
 
     void Start()
     {
         var videoPath = GetVideoPath();
         
-#if UNITY_EDITOR
-        if (!File.Exists(videoPath))
-        {
-            SwitchScene();
-            return;
-        }
-#endif
-        
         videoPlayer.url = videoPath;
         
-        videoPlayer.loopPointReached += EndReached;
-        videoPlayer.prepareCompleted += PlayVideo;
-        
         videoPlayer.Prepare();
+        
+        StartCoroutine(WaitForVideoPreparation());
     }
-    
+
+    private IEnumerator WaitForVideoPreparation()
+    {
+        float videoStartTime = Time.realtimeSinceStartup;
+        bool isVideoReady = false;
+        
+        while (Time.realtimeSinceStartup - videoStartTime < 5f && !isVideoReady)
+        {
+            if (videoPlayer.isPrepared)
+            {
+                isVideoReady = true;
+            }
+            
+            yield return null;
+        }
+
+        if (isVideoReady) yield break;
+        
+        SwitchScene();
+    }
     void PlayVideo(VideoPlayer video)
     {
         video.Play();
     }
     
-    void EndReached(VideoPlayer vp)
+    void EndReached(VideoPlayer video)
     {
-        vp.Stop();
+        video.Stop();
         SwitchScene();
     }
 
