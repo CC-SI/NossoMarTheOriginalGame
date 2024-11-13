cf4ae443ae52842d2f956e086750e13074eef6fb
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-11-12 21:34:21 -0300

    feat: criando uma classe para controlar a musica dos creditos

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    public static AudioController Instance { get; private set; }
+
+    void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    
+    public void PlaySong()
+    {
+        audioSource.Play();
+    }
+    
+    public void StopSong()
+    {
+        audioSource.Stop();
+    }
+
+#if UNITY_EDITOR
+    private void Reset()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+#endif
+}
