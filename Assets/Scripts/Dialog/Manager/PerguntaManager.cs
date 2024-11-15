38146888ea402a3620c496ddaa8b5add209d26b5
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-11-15 12:44:38 -0300

    feat : perguntas respondidas não aparecem mais, quando o usuario responder mais de 24 perguntas as perguntas já respondidas voltam a aparecer

--- Assets/Scripts/Dialog/Manager/PerguntaManager.cs
+++ Assets/Scripts/Dialog/Manager/PerguntaManager.cs
@@ -1,107 +1,185 @@
 ﻿using System.Collections.Generic;
+using Dialog;
+using Dialog.Manager;
 using Dialog.Pergunta;
 using Player;
 using UnityEngine;
 
-namespace Dialog.Manager
+public class PerguntaManager : MonoBehaviour
 {
-    public class PerguntaManager : MonoBehaviour
+    public static int perguntasRespondidasTotal = 0;
+
+    [SerializeField] private PerguntasUIManager perguntasUIManager;
+    [SerializeField] private DialogManager dialogManager;
+    [SerializeField] private DialogObject perguntaObject;
+    [SerializeField] private int numeroMaximoPerguntas = 3;
+    private List<DialogoPergunta> perguntasEmbaralhadas = new();
+    private List<PerguntaObject> perguntasObjectList = new();
+
+    [SerializeField] public bool isTutorialMove;
+
+    private int perguntaAtualIndex = 0;
+    private int acertos = 0;
+
+    public void Start()
     {
-        [SerializeField] private PerguntasUIManager perguntasUIManager;
-        [SerializeField] private DialogManager dialogManager;
-        [SerializeField] private DialogObject perguntaObject;
-        [SerializeField] private int numeroMaximoPerguntas = 3; 
-        private List<DialogoPergunta> perguntasEmbaralhadas = new();
+        CarregarPerguntasRespondidas();
 
+        perguntasUIManager.InitComponent();
+        StartPerguntas();
+    }
+    
+    private void OnApplicationQuit()
+    {
+        SalvarPerguntasRespondidas();
+    }
 
-        [SerializeField] public bool isTutorialMove;
-        
-        private int perguntaAtualIndex = 0;
-
-        private int acertos = 0;
-        
-        public void Start()
+    private void SalvarPerguntasRespondidas()
+    {
+        PlayerPrefs.SetInt("PerguntasRespondidasTotal", perguntasRespondidasTotal);
+        PlayerPrefs.Save();
+    }
+    
+    private void CarregarPerguntasRespondidas()
+    {
+        if (PlayerPrefs.HasKey("PerguntasRespondidasTotal"))
         {
-            perguntasUIManager.InitComponent();
+            perguntasRespondidasTotal = PlayerPrefs.GetInt("PerguntasRespondidasTotal");
+        }
+        else
+        {
+            perguntasRespondidasTotal = 0; 
+        }
+
+        Debug.Log("Perguntas respondidas ao iniciar: " + perguntasRespondidasTotal);
+    }
+
+    public void StartPerguntas()
+    {
+        if (perguntasRespondidasTotal >= 24)
+        {
+            ResetarPerguntasRespondidas();
         }
         
-        public void StartPerguntas()
-        {
-            ResetarPerguntas();
-        }
+        ResetarPerguntas();
+    }
 
-        public void AvancarPergunta()
+    public void AvancarPergunta()
+    {
+        if (perguntaAtualIndex < perguntasEmbaralhadas.Count - 1)
         {
-            if (perguntaAtualIndex < perguntasEmbaralhadas.Count - 1)
-            {
-                perguntaAtualIndex++;
-                ExibirPerguntaAtual();
-            } 
-            else
-            {
-                perguntasUIManager.ShowPainelPerguntas(false); 
-                
-                if (!isTutorialMove)
-                {
-                    PlayerBehaviour.Instance.Movement.enabled = true;
-                }
-                
-                if (acertos >= 2)
-                {
-                    dialogManager.AvancarDialogo();
-                } 
-                
-            }
-        }
-        
-        private void ExibirPerguntaAtual()
-        {
-            if (perguntaAtualIndex < perguntasEmbaralhadas.Count)
-            {
-                var dialogoAtual = perguntasEmbaralhadas[perguntaAtualIndex];
-                perguntasUIManager.AtualizarPergunta(dialogoAtual, this);
-            }
-        }
-        
-        public void ResetarPerguntas()
-        {
-            perguntaAtualIndex = 0;
-            perguntasEmbaralhadas = new List<DialogoPergunta>();
-
-            foreach (var dialogo in perguntaObject.Dialogos)
-            {
-                if (dialogo.pergunta != null)
-                {
-                    perguntasEmbaralhadas.AddRange(dialogo.pergunta.dialogosPergunta);
-                }
-            }
-            
-            EmbaralharPerguntas(perguntasEmbaralhadas);
-            
-            if (perguntasEmbaralhadas.Count > numeroMaximoPerguntas)
-            {
-                perguntasEmbaralhadas = perguntasEmbaralhadas.GetRange(0, numeroMaximoPerguntas);
-            }
-
-            perguntasUIManager.HabilitarBotoes();
+            perguntaAtualIndex++;
+            MarcarComoRespondida(perguntasEmbaralhadas[perguntaAtualIndex]);
+            Debug.Log("Id da pergunta: " + perguntasEmbaralhadas[perguntaAtualIndex].id);
             ExibirPerguntaAtual();
         }
-        
-        private void EmbaralharPerguntas(List<DialogoPergunta> perguntas)
+        else
         {
-            for (int i = perguntas.Count - 1; i > 0; i--)
+            perguntasUIManager.ShowPainelPerguntas(false);
+
+            if (!isTutorialMove)
             {
-                int j = Random.Range(0, i + 1);
-                var temp = perguntas[i];
-                perguntas[i] = perguntas[j];
-                perguntas[j] = temp;
+                PlayerBehaviour.Instance.Movement.enabled = true;
+            }
+
+            if (acertos >= 2)
+            {
+                dialogManager.AvancarDialogo();
+            }
+
+            MarcarComoRespondida(perguntasEmbaralhadas[perguntaAtualIndex]);
+        }
+
+        AtualizarPerguntasRespondidas();
+    }
+
+    private void AtualizarPerguntasRespondidas()
+    {
+        perguntasRespondidasTotal++;
+        Debug.Log("Total de perguntas respondidas por todas as instâncias: " + perguntasRespondidasTotal);
+    }
+
+    private void ExibirPerguntaAtual()
+    {
+        if (perguntaAtualIndex < perguntasEmbaralhadas.Count)
+        {
+            var dialogoAtual = perguntasEmbaralhadas[perguntaAtualIndex];
+            perguntasUIManager.AtualizarPergunta(dialogoAtual, this);
+        }
+    }
+
+    public void ResetarPerguntas()
+    {
+        perguntaAtualIndex = 0;
+        perguntasEmbaralhadas.Clear();
+
+        foreach (var dialogo in perguntaObject.Dialogos)
+        {
+            if (dialogo.pergunta != null)
+            {
+                foreach (var pergunta in dialogo.pergunta.dialogosPergunta)
+                {
+                    if (!pergunta.isRespondida) 
+                    {
+                        perguntasEmbaralhadas.Add(pergunta);
+                    }
+                }
             }
         }
-        
-        public int IncremetarAcertos()
+
+        EmbaralharPerguntas(perguntasEmbaralhadas);
+
+        if (perguntasEmbaralhadas.Count > numeroMaximoPerguntas)
         {
-            acertos++;
-            return acertos;
+            perguntasEmbaralhadas = perguntasEmbaralhadas.GetRange(0, numeroMaximoPerguntas);
         }
+
+        perguntasUIManager.HabilitarBotoes();
+        ExibirPerguntaAtual();
+
+        Debug.Log("Perguntas carregadas: " + perguntasEmbaralhadas.Count);
+    }
+
+    private void EmbaralharPerguntas(List<DialogoPergunta> perguntas)
+    {
+        for (int i = perguntas.Count - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            var temp = perguntas[i];
+            perguntas[i] = perguntas[j];
+            perguntas[j] = temp;
+        }
+    }
+    
+    private void ResetarPerguntasRespondidas()
+    {
+        foreach (var dialogo in perguntaObject.Dialogos)
+        {
+            if (dialogo.pergunta != null)
+            {
+                foreach (var pergunta in dialogo.pergunta.dialogosPergunta)
+                {
+                    if (pergunta.isRespondida)
+                    {
+                        pergunta.isRespondida = false;
+                        Debug.Log("Pergunta resetada: " + pergunta.id);
+                    }
+                }
+            }
+        }
+    
+        perguntasRespondidasTotal = 0; 
+        Debug.Log("Contador de perguntas respondidas resetado para 0.");
+    }
+
+    private void MarcarComoRespondida(DialogoPergunta perguntaRespondida)
+    {
+        perguntaRespondida.isRespondida = true;
+    }
+
+    public int IncremetarAcertos()
+    {
+        acertos++;
+        return acertos;
     }
 }
