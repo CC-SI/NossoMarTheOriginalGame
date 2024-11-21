59c4db981497e501bdc1e0d246d292b89a34463b
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-11-21 01:26:49 -0300

    feat: criando um controlador para a caixa de texto na cena de introdução

---
+++ Assets/Scripts/TextBoxController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Dialog;
+using UnityEngine;
+
+public class TextBoxController : MonoBehaviour
+{
+    [SerializeField] private List<Dialogo> duckIntroduction;
+    
+    private int dialogCount = 0;
+
+    private void Awake()
+    {
+        TextBox.OnTextEnded += NextDialog;
+    }
+
+    private void Start()
+    {
+        TextBox.Show(duckIntroduction[dialogCount]);
+    }
+    
+    private void NextDialog()
+    {
+        if (dialogCount < duckIntroduction.Count-1)
+        {
+            dialogCount++;
+            TextBox.Show(duckIntroduction[dialogCount]);
+            return;
+        }
+        
+        TextBox.OnTextEnded -= NextDialog;
+        GameManager.LoadTutorial();
+    }
+}
