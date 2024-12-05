7835ee87267dd99f84c1665bfc92c4a7a39313b3
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-12-05 10:52:28 -0300

    feat : sub classe de dialogo com decisão

---
+++ Assets/Scripts/Dialogos/Services/DialogWithChoices.cs
@@ -0,0 +1,56 @@
+﻿using Assets.Scripts.Dialogos.Enum;
+using Assets.Scripts.Dialogos.Modal;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Dialogos.Services
+{
+    public class DialogWithChoices : DialogoBase
+    {
+        [SerializeField] private Button btnSim;
+        [SerializeField] private Button btnNao;
+
+        protected override void Start()
+        {
+            base.Start();
+            
+            btnSim.gameObject.SetActive(false);
+            btnNao.gameObject.SetActive(false);
+        }
+
+        protected override void ShowDialogo()
+        {
+            base.ShowDialogo();
+            
+            if (dialogoObject.GetDialogoAt(index).TipoDialogoEnum == TipoDialogoEnum.Decisao)
+            {
+                btnSim.gameObject.SetActive(true);
+                btnNao.gameObject.SetActive(true);
+                botaoProximo.gameObject.SetActive(false);
+            } 
+            else
+            {
+                btnSim.gameObject.SetActive(false);
+                btnNao.gameObject.SetActive(false);
+                botaoProximo.gameObject.SetActive(true);
+            }
+        }
+
+        protected override void ListenToEvents()
+        {
+            base.ListenToEvents();
+            btnSim.onClick.AddListener(EscolherSim);
+            btnNao.onClick.AddListener(EscolherNao);
+        }
+
+        void EscolherSim()
+        {
+            NextDialogo(); 
+        }
+
+        void EscolherNao()
+        {
+            OcultarDialogo();
+        }
+    }
+}
