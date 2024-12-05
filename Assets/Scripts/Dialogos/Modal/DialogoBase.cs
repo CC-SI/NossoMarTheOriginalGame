6d5ff55f2bac7c538dc9e102bd438b3b88a6f8f3
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-12-05 10:52:04 -0300

    feat : classe abstrata de dialogos

---
+++ Assets/Scripts/Dialogos/Modal/DialogoBase.cs
@@ -0,0 +1,80 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Dialogos.Modal
+{
+    public abstract class DialogoBase : MonoBehaviour
+    {
+        [Header("Componentes UI")]
+        [SerializeField] private GameObject dialogoPainel;
+        [SerializeField] private TextMeshProUGUI orador;
+        [SerializeField] private TextMeshProUGUI texto;
+        [SerializeField] protected Button botaoProximo;
+        [SerializeField] private Button botaoFechar;
+        [SerializeField] private Button zonaDeFechar;
+
+        protected DialogoObject dialogoObject;
+        protected int index = 0;
+        bool dialogoAtivo = false;
+        
+        protected virtual void Start()
+        {
+            ListenToEvents();
+            
+            zonaDeFechar.gameObject.SetActive(false);
+        }
+        
+        public void SetDialogoObject(DialogoObject dialogo)
+        {
+            dialogoObject = dialogo;
+        }
+        
+        public void StartDialogo()
+        {
+            index = 0;
+            ShowDialogo();
+        }
+        
+        protected void NextDialogo()
+        {
+            index++;
+            ShowDialogo();
+            
+            if (index >= dialogoObject.dialogos.Count)
+            {
+                FinishedDialogo();
+                return;
+            }
+        }
+        
+        protected virtual void ShowDialogo()
+        {
+            dialogoPainel.SetActive(true);
+            orador.text = dialogoObject.GetDialogoAt(index).orador;
+            texto.text = dialogoObject.GetDialogoAt(index).texto;
+            dialogoAtivo = true;
+        }
+
+        protected void OcultarDialogo()
+        {
+            dialogoPainel.SetActive(false);
+            dialogoAtivo = false;
+        }
+        
+        void FinishedDialogo()
+        {
+            dialogoPainel.SetActive(false);
+            dialogoAtivo = false;
+            index = 0;
+        }
+        
+        protected virtual void ListenToEvents()
+        {
+            botaoProximo.onClick.AddListener(NextDialogo);
+            botaoFechar.onClick.AddListener(OcultarDialogo);
+            zonaDeFechar.onClick.AddListener(OcultarDialogo);
+        }
+        
+    }
+}
